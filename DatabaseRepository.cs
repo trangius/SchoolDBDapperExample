18183461dfa70ca27ac48ba71c339974769ec20d
krister <krister@trangius.se>
2024-11-01 15:23:36 +0100

    add project files

---
+++ DatabaseRepository.cs
@@ -0,0 +1,44 @@
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+class DatabaseRepository
+{
+    public IDbConnection Connect()
+    {
+        string connectionString = File.ReadAllText("connectionString.txt");
+        return new SqlConnection(connectionString);
+    }
+
+    public List<Student> GetAllStudents()
+    {
+        string sql = "SELECT Name, Email, DateOfBirth FROM Students";
+        using IDbConnection conn = Connect();
+        return conn.Query<Student>(sql).AsList();
+    }
+
+    public Student GetStudentById(int id)
+    {
+        string sql = "SELECT Name, Email, DateOfBirth FROM Students WHERE id=" + id;
+        //Console.WriteLine(sql);
+        using IDbConnection conn = Connect();
+        return conn.QuerySingle<Student>(sql);
+    }
+
+    public void AddStudent(string name, string email, string date)
+    {
+        string sql = $"INSERT INTO Students (Name, Email, DateOfBirth) VALUES('{name}', '{email}', '{date}')";
+        //Console.WriteLine(sql);
+        using IDbConnection conn = Connect();
+        conn.Execute(sql);
+    }
+
+    public List<(string Name, double AverageGrade)> GetAverageGradeForAllCourses()
+    {
+        string sql = @"SELECT Courses.Name, AVG(CAST(Enrollments.Grade AS FLOAT)) AS AverageGrade
+                        FROM Courses
+                        JOIN Enrollments ON Courses.Id = Enrollments.CourseId
+                        GROUP BY Courses.Id, Courses.Name;";
+        using IDbConnection conn = Connect();
+        return conn.Query<(string Name, double AverageGrade)>(sql).AsList();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// använd viktiga bibliotek för att Databasanslutning och Dapper ska funka
+class Student
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
+
+class Course
+{
+    public string Name {get;set;}
+    public int Credits{get;set;}
+    public string Teacher {get;set;}
+}
+
+class Programmmmmmmm
+{
+    static void Main()
+    {
+        DatabaseRepository repo = new DatabaseRepository();
+
+        List<(string Name, double AverageGrade)> avgGrades = repo.GetAverageGradeForAllCourses();
+        foreach(var avgGrade in avgGrades)
+        {
+            Console.WriteLine($"Kurs: {avgGrade.Name}, Medelbetyg: {avgGrade.AverageGrade}");
+        }
+        // List<Student> students = repo.GetAllStudents();
+        // foreach (Student student in students)
+        // {
+        //     Console.WriteLine($"Namn: {student.Name}, E-post: {student.Email}, Födelsedatum: {student.DateOfBirth}");
+        // }
+
+        // Console.Write("Vilken student vill du se? (Ange ID):");
+        // int id = int.Parse(Console.ReadLine());
+        // Student aStudent = repo.GetStudentById(id);
+        // Console.WriteLine($"Namn: {aStudent.Name}, E-post: {aStudent.Email}, Födelsedatum: {aStudent.DateOfBirth}");
+        
+        // Console.Write("Ange namn: ");
+        // string name = Console.ReadLine();
+        
+        // Console.Write("Ange Email: ");
+        // string email = Console.ReadLine();
+        
+        // Console.Write("Ange Födelsedatum: ");
+        // string date = Console.ReadLine();
+        // repo.AddStudent(name, email, date);
+
+    }
+}
